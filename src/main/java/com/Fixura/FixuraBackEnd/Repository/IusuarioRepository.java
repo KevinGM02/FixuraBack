8182d552f5d2e2cbd46ec494c6b3eee453c0ce5a
Pablo AQ <alfaroquispepablo@gmail.com>
2024-09-16 02:56:21 -0500

    Service, Repository and Controller for Login Usuario

--- src/main/java/com/Fixura/FixuraBackEnd/Repository/IusuarioRepository.java
+++ src/main/java/com/Fixura/FixuraBackEnd/Repository/IusuarioRepository.java
@@ -1,7 +1,9 @@
 package com.Fixura.FixuraBackEnd.Repository;
 
+
 import com.Fixura.FixuraBackEnd.Model.Usuario;
 
 public interface IusuarioRepository {
   public int save(Usuario usuario);
+  public Usuario login(Usuario usuario);
 }
--- src/main/java/com/Fixura/FixuraBackEnd/Service/IusuarioService.java
+++ src/main/java/com/Fixura/FixuraBackEnd/Service/IusuarioService.java
@@ -2,6 +2,8 @@ package com.Fixura.FixuraBackEnd.Service;
 
 import com.Fixura.FixuraBackEnd.Model.Usuario;
 
+
 public interface IusuarioService {
   public int save(Usuario usuario);
+  public Usuario login(Usuario usuario);
 }
